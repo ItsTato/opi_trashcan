04ad92af7fc462dd9689d9de139c3010485e8859
ItsTato <itstato.official@gmail.com>
2024-04-23 14:51:16 -0600

    LOL

--- test.cpp
+++ test.cpp
@@ -4,8 +4,9 @@
 int LED_1 = 10;
 int LED_2 = 13;
 int LED_3 = 16;
+int LED_4 = 3;
 
-int WAIT_TIME_MS = 500;
+int WAIT_TIME_MS = 350;
 
 int main() {
     wiringPiSetup();
@@ -13,23 +14,33 @@ int main() {
     pinMode(LED_1,OUTPUT);
     pinMode(LED_2,OUTPUT);
     pinMode(LED_3,OUTPUT);
+    pinMode(LED_4,OUTPUT);
 
     while(1) {
         digitalWrite(LED_1,HIGH);
         digitalWrite(LED_2,HIGH);
         digitalWrite(LED_3,HIGH);
+        digitalWrite(LED_4,HIGH);
         delay(WAIT_TIME_MS);
         digitalWrite(LED_1,HIGH);
         digitalWrite(LED_2,LOW);
         digitalWrite(LED_3,LOW);
+        digitalWrite(LED_4,LOW);
         delay(WAIT_TIME_MS);
         digitalWrite(LED_1,LOW);
         digitalWrite(LED_2,HIGH);
         digitalWrite(LED_3,LOW);
+        digitalWrite(LED_4,LOW);
         delay(WAIT_TIME_MS);
         digitalWrite(LED_1,LOW);
         digitalWrite(LED_2,LOW);
         digitalWrite(LED_3,HIGH);
+        digitalWrite(LED_4,LOW);
+        delay(WAIT_TIME_MS);
+        digitalWrite(LED_1,LOW);
+        digitalWrite(LED_2,LOW);
+        digitalWrite(LED_3,LOW);
+        digitalWrite(LED_4,HIGH);
         delay(WAIT_TIME_MS);
         digitalWrite(LED_1,LOW);
         digitalWrite(LED_2,LOW);
