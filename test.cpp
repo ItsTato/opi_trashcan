71e4b1098cf0da55b06d4234f4d13715e32bd773
ItsTato <itstato.official@gmail.com>
2024-04-23 14:45:36 -0600

    Three leds!

--- test.cpp
+++ test.cpp
@@ -3,27 +3,39 @@
 // sudo gcc -o test test.cpp -l wiringPi
 int LED_1 = 10;
 int LED_2 = 13;
+int LED_3 = 16;
+
+int WAIT_TIME_MS = 500;
 
 int main() {
     wiringPiSetup();
 
     pinMode(LED_1,OUTPUT);
     pinMode(LED_2,OUTPUT);
+    pinMode(LED_3,OUTPUT);
 
     while(1) {
         digitalWrite(LED_1,HIGH);
         digitalWrite(LED_2,HIGH);
-        delay(1000);
+        digitalWrite(LED_3,HIGH);
+        delay(WAIT_TIME_MS);
         digitalWrite(LED_1,HIGH);
         digitalWrite(LED_2,LOW);
-        delay(1000);
+        digitalWrite(LED_3,LOW);
+        delay(WAIT_TIME_MS);
         digitalWrite(LED_1,LOW);
         digitalWrite(LED_2,HIGH);
-        delay(1000);
+        digitalWrite(LED_3,LOW);
+        delay(WAIT_TIME_MS);
         digitalWrite(LED_1,LOW);
         digitalWrite(LED_2,LOW);
-        delay(1000);
+        digitalWrite(LED_3,HIGH);
+        delay(WAIT_TIME_MS);
+        digitalWrite(LED_1,LOW);
+        digitalWrite(LED_2,LOW);
+        digitalWrite(LED_3,LOW);
+        delay(WAIT_TIME_MS);
     }
-// GitHub
+
     return 1;
 }
