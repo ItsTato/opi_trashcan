87541af871e4cf43e3890cddfaf00bd47c231113
ItsTato <itstato.official@gmail.com>
2024-04-23 14:30:53 -0600

    mas cool

--- test.cpp
+++ test.cpp
@@ -12,12 +12,18 @@ int main() {
 
     while(1) {
         digitalWrite(LED_1,HIGH);
+        digitalWrite(LED_2,HIGH);
+        delay(1000);
+        digitalWrite(LED_1,HIGH);
         digitalWrite(LED_2,LOW);
         delay(1000);
         digitalWrite(LED_1,LOW);
         digitalWrite(LED_2,HIGH);
         delay(1000);
+        digitalWrite(LED_1,LOW);
+        digitalWrite(LED_2,LOW);
+        delay(1000);
     }
-
+// GitHub
     return 1;
 }
